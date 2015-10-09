669a2e0d353122ac825d98af37f4f5566f028b45
ShermanDrake <shermandrake@comcast.net>
2015-10-09 11:36:18 -0600

    second commit

--- problemset4.js
+++ problemset4.js
@@ -23,23 +23,77 @@
 // 	},0)
 
 // }
-// console.log(addNumbers("88hello3 world44"))
+// console.log(addNumbers("Hello6 9World 2, Nic8e D7ay!"))
 
 
 
-var longestWord = function(string) {
-	var string = string.split(" ");
-	var longest = 0;
-	var word = null;
+// var longestWord = function(string) {
+// 	var string = string.split(" ");
+// 	var longest = 0;
+// 	var word = null;
 
-	for( var i=0; i<=string.length - 1; i++){
+// 	for( var i=0; i<=string.length - 1; i++){
 
-		if(longest < string[i].length) {
-			longest = string[i].length;
-			word = string[i];
+// 		if(longest < string[i].length) {
+// 			longest = string[i].length;
+// 			word = string[i];
+// 		}
+// 	}
+// 	return word;
+
+// }
+// console.log(longestWord("The Bronco's play Da'Raiders on Sunday"))
+
+
+var letters =[]
+var digits =[]
+
+var averageStringNumbers = function(number) {
+	var myloop = number
+
+	for( i=0; i<myloop.length; i++){
+		// console.log(parseInt(myloop[i], 10))
+
+		if(!isNaN(parseInt(myloop[i],10))&& !isNaN(parseInt(myloop[i+1], 10))){
+			// console.log('!!',myloop[i], parseInt(myloop[i],10))
+
+		digits.push(myloop[i]+ myloop[i+1])
+		i++
+		}
+		else if(!isNaN(parseInt(myloop[i], 10))){
+			digits.push(myloop[i])
+
+		}
+		else {
+			letters.push(myloop[i])
 		}
 	}
-	return word;
+
+
+	var sum = digits.reduce(function(memo,element){
+		return memo += parseInt(element)
+
+	},0)
+
+	return sum / letters.length
 
 }
-console.log(longestWord("The Bronco's play Da'Raiders on Sunday"))
+console.log(averageStringNumbers("Hello6 9World 2, Nic8e D7ay!"))
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
